f680dc55306e7f3cc0d2593fb0fcdd657552415c
Ian Rufus <ian.j.rufus@gmail.com>
2016-07-21 23:56:05 +0100

    Added method stubs for remaining calls

--- src/Zoopla.Net/ZooplaDotNetClient.cs
+++ src/Zoopla.Net/ZooplaDotNetClient.cs
@@ -34,5 +34,75 @@ namespace Zoopla.Net
 
             return await _httpClient.GetObject<AverageSoldPrices>(url);
         }
+
+        public async Task<string> GetPropertyRichList()
+        {
+            string url = Endpoints.PROPERTY_RICH_LIST + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetAverageAreaSoldPrice()
+        {
+            string url = Endpoints.AVERAGE_AREA_SOLD_PRICE + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetZedIndex()
+        {
+            string url = Endpoints.ZED_INDEX + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetZedIndices()
+        {
+            string url = Endpoints.ZED_INDICES + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetAreaValueGraphs()
+        {
+            string url = Endpoints.AREA_VALUE_GRAPHS + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetSessionId()
+        {
+            string url = Endpoints.SESSION_ID + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetRefineEstimate()
+        {
+            string url = Endpoints.REFINE_ESTIMATE + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> ArrangeViewing()
+        {
+            string url = Endpoints.ARRANGE_VIEWING + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetLocalInfoGraphs()
+        {
+            string url = Endpoints.LOCAL_INFO_GRAPHS + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetGeoAutocomplete()
+        {
+            string url = Endpoints.GEO_AUTOCOMPLETE + "?api_key=" + _accessToken;
+
+            throw new NotImplementedException();
+        }
     }
 }
