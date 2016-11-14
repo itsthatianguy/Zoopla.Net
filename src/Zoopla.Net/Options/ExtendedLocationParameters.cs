1f07bbb292cbe063fe300dc7a0bd8fa6a8d78883
Ian.Rufus <Ian.Rufus@WAKLAP-415122>
2016-11-14 15:58:31 +0000

    Standard extracting out standard location options

---
+++ src/Zoopla.Net/Options/ExtendedLocationParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoopla.Net.Options
+{
+    public class ExtendedLocationParameters : StandardLocationParameters
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float LatMax { get; set; }
+        public float LatMin { get; set; }
+        public float LonMax { get; set; }
+        public float LonMin { get; set; }
+
+        public void GetUrlString()
+        {
+            UrlValues = new Dictionary<string, string>();
+
+            base.AddProperties();
+            AddProperties();
+
+            base.ConstructUrl();
+        }
+
+        internal void AddProperties()
+        {
+            base.AddProperties();
+        }
+    }
+}
---
+++ src/Zoopla.Net/Options/StandardLocationParameters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoopla.Net.Options
+{
+    public class StandardLocationParameters
+    {
+        internal string Url;
+        internal Dictionary<string, string> UrlValues { get; set; }
+
+        public string Area { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+        public string County { get; set; }
+        public string Country { get; set; }
+        public OutputType OutputType { get; set; }
+
+        public string GetUrlString()
+        {
+            UrlValues = new Dictionary<string, string>();
+
+            AddProperties();
+
+            return ConstructUrl();
+        }
+
+        internal void AddProperties()
+        {
+            UrlValues.Add("area", Area?.ToLower());
+            UrlValues.Add("postcode", PostCode?.ToLower());
+            UrlValues.Add("street", Street?.ToLower());
+            UrlValues.Add("town", Town?.ToLower());
+            UrlValues.Add("county", County?.ToLower());
+            UrlValues.Add("country", Country?.ToLower());
+            UrlValues.Add("output_type", OutputType.ToString()?.ToLower());
+        }
+
+        internal string ConstructUrl()
+        {
+            // Construct url from dictionary
+            foreach (KeyValuePair<string, string> keyValue in UrlValues)
+            {
+                // Some values can be 0...
+                if (keyValue.Value != String.Empty &&
+                    keyValue.Value != null &&
+                    keyValue.Value != "0")
+                {
+                    Url += "&" + keyValue.Key + "=" + keyValue.Value;
+                }
+            }
+
+            return Url;
+        }
+    }
+}
