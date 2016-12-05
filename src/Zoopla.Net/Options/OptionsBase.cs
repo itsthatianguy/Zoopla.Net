98b4f17108f00f53dd42882938995ddb89dcbd53
Ian Rufus <ian.j.rufus@gmail.com>
2016-12-05 20:11:46 +0000

    Created a base class for common options properties and methods. Made use of the options on the standard location parameters

---
+++ src/Zoopla.Net/Options/OptionsBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoopla.Net.Options
+{
+    public class OptionsBase
+    {
+        internal NameValueCollection UrlValues { get; set; }
+
+        public OptionsBase()
+        {
+            UrlValues = new NameValueCollection();
+        }
+
+        public string GetUrlParams()
+        {
+            var array = (from key in UrlValues.AllKeys
+                         from value in UrlValues.GetValues(key)
+                         select string.Format("{0}={1}", key, value))
+                        .ToArray();
+            return "&" + string.Join("&", array);
+        }
+    }
+}
--- src/Zoopla.Net/Options/StandardLocationParameters.cs
+++ src/Zoopla.Net/Options/StandardLocationParameters.cs
@@ -5,54 +5,56 @@ using System.Threading.Tasks;
 
 namespace Zoopla.Net.Options
 {
-    public class StandardLocationParameters
+    public class StandardLocationParameters : OptionsBase
     {
-        internal string Url;
-        internal Dictionary<string, string> UrlValues { get; set; }
-
-        public string Area { get; set; }
-        public string Street { get; set; }
-        public string Town { get; set; }
-        public string PostCode { get; set; }
-        public string County { get; set; }
-        public string Country { get; set; }
-        public OutputType OutputType { get; set; }
-
-        public string GetUrlString()
+        public string Area
         {
-            UrlValues = new Dictionary<string, string>();
-
-            AddProperties();
-
-            return ConstructUrl();
-        }
-
-        internal void AddProperties()
-        {
-            UrlValues.Add("area", Area?.ToLower());
-            UrlValues.Add("postcode", PostCode?.ToLower());
-            UrlValues.Add("street", Street?.ToLower());
-            UrlValues.Add("town", Town?.ToLower());
-            UrlValues.Add("county", County?.ToLower());
-            UrlValues.Add("country", Country?.ToLower());
-            UrlValues.Add("output_type", OutputType.ToString()?.ToLower());
-        }
-
-        internal string ConstructUrl()
-        {
-            // Construct url from dictionary
-            foreach (KeyValuePair<string, string> keyValue in UrlValues)
+            set
             {
-                // Some values can be 0...
-                if (keyValue.Value != String.Empty &&
-                    keyValue.Value != null &&
-                    keyValue.Value != "0")
-                {
-                    Url += "&" + keyValue.Key + "=" + keyValue.Value;
-                }
+                UrlValues["area"] = value?.ToLower();
             }
-
-            return Url;
+        }
+        public string Street
+        {
+            set
+            {
+                UrlValues["street"] = value?.ToLower();
+            }
+        }
+        public string Town
+        {
+            set
+            {
+                UrlValues["town"] = value?.ToLower();
+            }
+        }
+        public string PostCode
+        {
+            set
+            {
+                UrlValues["postcode"] = value?.ToLower();
+            }
+        }
+        public string County
+        {
+            set
+            {
+                UrlValues["county"] = value?.ToLower();
+            }
+        }
+        public string Country
+        {
+            set
+            {
+                UrlValues["country"] = value?.ToLower();
+            }
+        }
+        public OutputType OutputType
+        {
+            set
+            {
+                UrlValues["output_type"] = value.ToString()?.ToLower();
+            }
         }
     }
 }
