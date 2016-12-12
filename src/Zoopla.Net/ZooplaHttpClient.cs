5aab28a0fb0c19869bf54ef40967732da99fc14d
Ian.Rufus <Ian.Rufus@WAKLAP-415122>
2016-12-12 12:12:45 +0000

    Updated http client to create a new response and set the error values if the response was unsuccessful

--- src/Zoopla.Net/ZooplaHttpClient.cs
+++ src/Zoopla.Net/ZooplaHttpClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Zoopla.Net.Models;
 
 namespace Zoopla.Net
 {
@@ -12,7 +13,7 @@ namespace Zoopla.Net
         // Only have one instance which get's re-used (https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/)
         private static HttpClient _client = new HttpClient();
 
-        public async Task<T> GetObject<T>(string url)
+        public async Task<T> GetObject<T>(string url) where T : ResponseModelBase
         {
             _client.DefaultRequestHeaders.Accept.Clear();
             HttpResponseMessage response = await _client.GetAsync(url);
@@ -24,9 +25,11 @@ namespace Zoopla.Net
             }
             else
             {
-                // Error?
+                T errorResponse = (T)Activator.CreateInstance(typeof(T));
+                errorResponse.ErrorString = await response.Content.ReadAsStringAsync();
+                errorResponse.ErrorCode = response.StatusCode.ToString();
+                return errorResponse;
             }
-            return default(T);
         }
     }
 }
