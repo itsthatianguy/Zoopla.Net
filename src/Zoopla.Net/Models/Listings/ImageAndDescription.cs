e0e74d771aa87c1a42a3897540f6e6833e333372
clockworkmice <clockworkmice@hotmail.com>
2017-06-16 22:39:02 +0100

    Adding undocumented but useful fields in arrange viewing response and changing some types to int,DateTime from string etc

---
+++ src/Zoopla.Net/Models/Listings/ImageAndDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Zoopla.Net.Models.Listings
+{
+    public class ImageAndDescription
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
